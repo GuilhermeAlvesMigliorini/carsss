40a6617847442e9d567740d30ce2a7207e91e4f4
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-04-26 11:58:49 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import next from 'next'
+import styles from '../styles/Test.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.ch1}>
+      <h1 >Home</h1>
+    </div>
+  )
+}
